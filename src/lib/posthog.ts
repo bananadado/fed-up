fd8955c21e1a196da898929ccd639c851339da67
Dorian Turner <dorianemmanuelturner@gmail.com>
2026-05-28 15:59:52 +0100

    Fix PostHog session replay startup

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -1,4 +1,6 @@
+import "posthog-js/dist/recorder";
 import posthogJs, { type PostHog } from "posthog-js";
+import { createPostHogConfig } from "./posthogConfig";
 
 declare const __BUN_PUBLIC_POSTHOG_PROJECT_TOKEN__: string | undefined;
 declare const __BUN_PUBLIC_POSTHOG_HOST__: string | undefined;
@@ -19,63 +21,8 @@ const host = readBunPublicEnv(
   "BUN_PUBLIC_POSTHOG_HOST",
 );
 
-const personalDataProperties = [
-  "email",
-  "name",
-  "postcode",
-  "postal_code",
-  "address",
-  "phone",
-  "sessionId",
-  "anonymous_session_id",
-];
-
 if (token) {
-  posthogJs.init(token, {
-    api_host: host || "https://app.posthog.com",
-    defaults: "2026-01-30",
-    capture_pageview: "history_change",
-    capture_pageleave: true,
-    autocapture: {
-      dom_event_allowlist: ["click", "change", "submit"],
-      element_allowlist: ["a", "button", "form", "input", "select", "textarea", "label"],
-      capture_copied_text: false,
-      element_attribute_ignorelist: ["value", "placeholder", "data-value"],
-    },
-    rageclick: {
-      click_count: 3,
-      threshold_px: 30,
-      timeout_ms: 1000,
-      content_ignorelist: true,
-    },
-    capture_dead_clicks: true,
-    capture_exceptions: true,
-    capture_heatmaps: true,
-    capture_performance: {
-      network_timing: true,
-      web_vitals: true,
-    },
-    disable_session_recording: false,
-    session_recording: {
-      maskAllInputs: true,
-      maskTextSelector: "[data-ph-mask]",
-      blockSelector: "[data-ph-block]",
-      recordHeaders: false,
-      recordBody: false,
-      maskCapturedNetworkRequestFn: request => ({
-        ...request,
-        requestHeaders: undefined,
-        requestBody: undefined,
-        responseHeaders: undefined,
-        responseBody: undefined,
-      }),
-    },
-    mask_personal_data_properties: true,
-    custom_personal_data_properties: personalDataProperties,
-    property_denylist: ["postcode", "sessionId"],
-  });
-
-  posthogJs.startSessionRecording(true);
+  posthogJs.init(token, createPostHogConfig(host));
 }
 
 type AnalyticsProperty = string | number | boolean | null | string[] | number[] | boolean[];
